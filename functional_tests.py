09b0b685c1b5e6fa5b22794a92e34d1ef2c6e5e9
Frederick Diehl <fred@Fredericks-MacBook-Air.local>
2018-01-11 20:18:41 -0600

    functional test now checks we can unput a todo item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -13,13 +15,29 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
         #check out the page title
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
 
+        self.assertIn('To-Do', header_text)
+
+        self.assertEqual(
+            iputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+    
         #types "buy peacock feathers" into a text box. 
+        inputbox.send_keys('Buy peakcock feathers')
 
         #when she hits enter the page updates and now lists "1: buys peackock feathers"
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows  = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peakcock feathers for row in rows')
+        )
         #there is still a text box inviting her to enter another item
+        self.fail('Finish the test')
 
         #enters "use feathers to make a fly"
 
