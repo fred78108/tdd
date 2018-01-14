9c49703479659367cc1293019cbb748c98948569
Frederick Diehl <fred@Fredericks-MacBook-Air.local>
2018-01-13 18:33:46 -0600

    created a helper function in functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -10,7 +10,12 @@ class NewVisitorTest(unittest.TestCase):
     
     def tearDown(self):
         self.browser.quit()
-    
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')  
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         #check out the page title
@@ -26,21 +31,20 @@ class NewVisitorTest(unittest.TestCase):
         )
     
         #types "buy peacock feathers" into a text box. 
-        inputbox.send_keys('Buy peakcock feathers')
+        inputbox.send_keys('Buy peacock feathers')
 
         #when she hits enter the page updates and now lists "1: buys peackock feathers"
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows  = table.find_elements_by_tag_name('tr')
         #self.assertTrue(
         #    any(row.text == '1: Buy peakcock feathers' for row in rows), "New to-do item did not appear in table"
         #    f"new to-do item did not appear in table. Contents were:\n{table.text}"
         #)
         # the above lines can be replaced with a single line, nice!
-        self.assertIn('1: Buy peakcock feathers', [row.text for row in rows]) 
+        #self.assertIn('1: Buy peakcock feathers', [row.text for row in rows]) 
 
 
         #there is still a text box inviting her to enter another item
@@ -50,13 +54,9 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         #the page updates again with both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows  = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peakcock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers')
+
         #the site has generated a unique URL. 
         self.fail('Finish the test')
 
