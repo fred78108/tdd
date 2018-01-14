ba1708efbf033f515e1990064b354e6d4f548933
Frederick Diehl <fred@Fredericks-MacBook-Air.local>
2018-01-14 10:00:09 -0600

    Redirect after POST and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -1,16 +1,34 @@
 from django.test import TestCase
 from lists.models import Item
 
-class HomePageTest(TestCase):
-
+class HomePageTest(TestCase):    
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
     def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={'item_text': 'a new list item'})
-        self.assertIn('a new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+        response = self.client.post('/', data={'item_text': 'a new list item'})        
+        self.assertEqual(Item.objects.count(),1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'a new list item')
+    
+    def test_redirects_after_POST(self):        
+        response = self.client.post('/', data={'item_text': 'a new list item'})        
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(),0)
+    
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
@@ -23,7 +41,7 @@ class ItemModelTest(TestCase):
         second_item.save()
 
         saved_items = Item.objects.all()
-        self.asserEqual(saved_items.count(),2)
+        self.assertEqual(saved_items.count(),2)
 
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
