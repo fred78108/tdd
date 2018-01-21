fca38742a08dac727abe4f09b62d78d515292f1b
Frederick Diehl <fred@Fredericks-MacBook-Air.local>
2018-01-21 08:50:42 -0600

    adjust new list view to do model validation

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -34,19 +34,47 @@ class ItemValidationTest(StaticLiveServerTestCase):
                 if time.time() - start_time > MAX_WAIT:
                     raise e
                 time.sleep(0.5)
+
+    def wait_for(self, fn):
+        start_time =time.time()
+        while True:
+            try:                
+                return fn()
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)                
     #@skip
     def  test_cannot_add_empty_list_items(self):
         #hit enter on an empty input box
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         #the page refreshes and there is an error that the list item 
         #cannot be blank
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        ))
 
         #tries again with text, it works
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         #now tries again to enter a blank item
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         #confirm another error message
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        ))
 
         #and corrects by filling in text
-        self.fail('write-me')
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        sef.wait_for_row_in_list_table('1: Buy milk')
+        sef.wait_for_row_in_list_table('2: Make tea')
+
+
         
